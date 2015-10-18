12910aef81efa0c4822a75939db61e53dc832517
Benjamin Tan <benjamintan@Benjamins-iMac.local>
2015-10-18 16:12:59 +0800

    Add missing :global

--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -2,7 +2,7 @@ defmodule Chucky.Supervisor do
   use Supervisor
 
   def start_link do
-    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+    Supervisor.start_link(__MODULE__, :ok, name: {:global, __MODULE__})
   end
 
   def init(:ok) do
